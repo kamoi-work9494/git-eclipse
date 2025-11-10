62d7e82a24556b6e0ec455b957709293fcf908be
Kamoi <kamoi.work9494@gmail.com>
2025-11-10 16:14:50 +0900

    a

--- src/pa/test.java
+++ src/pa/test.java
@@ -2,6 +2,6 @@ package pa;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!!編集。local");
+		System.out.println("Hello, World!!編集。locala");
 	}
 }
